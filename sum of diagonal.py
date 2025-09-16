cb994b994fd7e7c386b41b3dfa46ff036ce63187
Rohithmaddipatla <rohithmaddipatla@gmail.com>
2025-09-16 15:15:08 +0530

    Add files via upload

---
+++ sum of diagonal.py
@@ -0,0 +1,18 @@
+'''
+matrix=[1,5,3,7,6,4,2,8,9]
+first_dia=matrix[0]
+second_dia=matrix[4]
+third_dia=matrix[8]
+sum=first_dia+second_dia+third_dia
+print("diagonal elements are:",first_dia,",",second_dia,",",third_dia)
+print("sum of diagonal elements =",sum)
+'''
+
+
+matrix=[1,2,3,4,5,6,2,4,5]
+dia1=matrix[0]
+dia2=matrix[4]
+dia3=matrix[8]
+sum=dia1+dia2+dia3
+print("diagonal elements :",dia1,",",dia2,",",dia3)
+print("sum of te diagonal:",sum)
